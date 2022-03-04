2c8029dbe9821f481d9e3960d3e757d7f2c257b0
Stan Strum <stanleystrum@gmail.com>
2022-03-03 23:48:27 -0800

    Remove duplicate test case

--- tests/multiple_ips.js
+++ tests/multiple_ips.js
@@ -3,8 +3,8 @@ process.on("unhandledRejection", e => console.error("Unhandled Promise Rejection
 const servers = [
   ["mc.hypixel.net", 25565],
   // ["192.168.1.3", 25565],
-  ["pe.mineplex.com", 19132],
   ["play.lbsg.net", 19132],
+  ["us.mineplex.com", 25565],
   ["pe.mineplex.com", 19132],
   ["192.168.1.3", 25566],
   ["hub.mc-complex.com", ],
